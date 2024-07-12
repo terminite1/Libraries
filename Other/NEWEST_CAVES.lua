aaa54054f9a5710bd21d32d72d515f32fc9f9f6a
terminitelol <86270641+terminite1@users.noreply.github.com>
2024-07-12 20:27:23 +0200

    Add files via upload

---
+++ Other/NEWEST_CAVES.lua
@@ -0,0 +1,361 @@
+-- Decompiled with Konstant, a fast Luau decompiler made in Luau by plusgiant5
+-- Decompiled on 2024-06-21 14:23:57
+-- Time taken: 0.001044 seconds
+
+return {
+	WE23 = {
+		Softsnow = {
+			Rarity = 12;
+			Ores = {
+				Frigishard = 475000;
+				Snowball = 52000;
+				Divinis = 55377324;
+				Astralisium = 700000;
+				Glaciare = 39500;
+				Glacius = 8765432;
+				Starrine = 41414;
+				Snowsled = 3666666;
+				["Glacial Shard"] = 25;
+				Snowfall = 85000;
+				Frigite = 900;
+			};
+			MainOre = "Packed Snow";
+		};
+		Fireplace = {
+			MainOre = "Latremite";
+			Ores = {
+				Jollinyte = 625000;
+				Starbrite = 13333;
+				Pitcoal = 20;
+				Toyblast = 28825000;
+				Blizzardine = 505505;
+				Mistletide = 260000;
+				Fernium = 5555;
+				Luxe = 32323232;
+			};
+			Rarity = 33;
+		};
+		Peppermint = {
+			Rarity = 25;
+			Ores = {
+				Peppernite = 333333;
+				Pentaurunel = 12000250;
+				["Yuletide Star"] = 77777777;
+				Candycore = 29000;
+				["Ice Fragment"] = 8350;
+				Ephemryst = 222222222;
+				Teslacane = 24000;
+				["Packed Ice"] = 100;
+				Peppermist = 155555;
+			};
+			MainOre = "Rubviridite";
+		};
+	};
+	W2 = {
+		Gilded = {
+			Rarity = 5700;
+			Ores = {
+				Gold = 20;
+				["Hyperheated Quasar"] = 810000;
+			};
+			MainOre = "Goldstone";
+		};
+		Galactic = {
+			MainOre = "Galaxistone";
+			Ores = {
+				Ignacaelum = 317278;
+				["Protoplanetary Disk"] = 4800;
+				["O-Star Fragment"] = 180;
+				["M-Star Fragment"] = 30;
+				["Messier 92"] = 31920;
+				["G-Star Fragment"] = 90;
+				Cygnus = 10480900;
+				["Galactic Rupture"] = 19282800;
+				Rigel = 11368000;
+				Estrela = 7429000;
+				Radisol = 2828;
+				["Cosmic Treasure"] = 2314909;
+				["Coronal Flare"] = 13150000;
+				["Quasar 618"] = 505505505;
+			};
+			Rarity = 16;
+		};
+		Nightfall = {
+			Ores = {
+				["Arctic Crystal"] = 9191;
+				Nemolite = 432109;
+				["The Nightmare"] = 621940;
+				Aquintite = 24790;
+				Aurutine = 1170;
+				Fracidial = 532000;
+				["NOO P \xCE\xB1"] = 631280500;
+				Lunarian = 411000;
+				["Twilight Mist"] = 13817000;
+				Culindrene = 63126;
+				Eboxylite = 18384;
+			};
+			MainOre = "Lunalistone";
+			Rarity = 43;
+		};
+		Luminous = {
+			MainOre = "Lumilux";
+			Rarity = 34;
+			Ores = {
+				["The Dream"] = 429400;
+				Lavortia = 1347290;
+				Lecilux = 38300;
+				Auroralium = 211290;
+				Citrine = 19230;
+				Luminaweb = 44440;
+				Icarus = 21736000;
+				Nitoril = 357000;
+				Shimmersite = 71900;
+				Scapolite = 2050;
+				Coselnix = 9282000;
+				Heidalium = 18395;
+			};
+		};
+		Enchanted = {
+			MainOre = "Lepidustone";
+			Ores = {
+				["Crystal Oculite"] = 2428200;
+				Violixium = 200750;
+				Argon = 39772;
+				["Unearthly Cubes"] = 26179938;
+				Shroomite = 17200;
+				Entiniol = 5500550;
+				Lotivium = 81430;
+				Xenon = 47270;
+				Fortinium = 146500;
+				Fluorite = 171880;
+				Asterium = 361000;
+				Benitoite = 30303;
+				Spiritian = 9682000;
+				Azurite = 4544;
+			};
+			Rarity = 22;
+		};
+		Unstable = {
+			Rarity = 13;
+			Ores = {
+				Exdeus = 32372000;
+				Gravitaticor = 44299000;
+				Radon = 35945;
+				["Unsteady Filaments"] = 17182490;
+				Condensium = 65590;
+				Plerivium = 36000;
+				Atomium = 6022140;
+				Calite = 173000;
+				Versulium = 227480;
+				["Continuum Rift"] = 37291000;
+				Protonium = 154990;
+				Phendium = 43660;
+				Plasmal = 305000;
+				Accretite = 3221110;
+			};
+			MainOre = "Destabilite";
+		};
+	};
+	SW1 = {
+		Radioactive = {
+			Rarity = 11;
+			MainOre = "Lunar Uranium";
+			Ores = {
+				Oganesson = 12400000;
+				["Lunar Lawrencium"] = 6000000;
+				["Lunar Promethium"] = 210000;
+				Actinium = 120000;
+				Surgium = 975000;
+				["Lunar Coronium"] = 600000;
+				["Lunar Newtonium"] = 200000;
+				["Lunar Astatine"] = 8500000;
+				["Lunar Poiseon"] = 833333;
+				Lutetium = 1750000;
+				["Lunar Neptunium"] = 800000;
+			};
+		};
+		Interstellar = {
+			Ores = {
+				["Lunar Quasar 618"] = 514300000;
+				["Laniakea Supercluster"] = 50000000;
+				["Lunar Andromidium"] = 3000000;
+				["HD 160529"] = 16000000;
+				R136a1 = 5300000;
+				["Lunar Aurora"] = 15000000;
+				["Lunar HR 5171 A"] = 9000000;
+				["Lunar Galaxite"] = 8900;
+				["Lunar Pulsar Crystal"] = 850000;
+				Protoflare = 12000000;
+				["Vaporwave Pulsar"] = 15900000;
+				["Sagittarius Quasar"] = 5600000;
+				Tektite = 10000;
+			};
+			MainOre = "Nebula";
+			Rarity = 11;
+		};
+		nil = {
+			Ores = {
+				Pandorite = 3400000;
+				Accesinite = 3000000;
+				NILNAL = 40000000;
+				["Fire Crystal"] = 3300000;
+				["Orb of Discontent"] = 10000000;
+				Genuinium = 3200000;
+			};
+			MainOre = "nil";
+			Rarity = 48;
+		};
+		Gilded = {
+			MainOre = "Goldstone";
+			Ores = {
+				Ambrosia = 111112;
+				Gold = 20;
+			};
+			Rarity = 5700;
+		};
+		Magmatic = {
+			MainOre = "Magma";
+			Rarity = 11;
+			Ores = {
+				["Ascended Flare"] = 6000000;
+				["Lunar Dyronsinite"] = 450000;
+				["Lunar Coronal Flare"] = 13150000;
+				Armageddium = 175000000;
+				["Lunar \xCE\xA9"] = 35000000;
+				["Lunar Gargantium"] = 6500000;
+				["Lunar Flaeon"] = 833333;
+			};
+		};
+	};
+	W1 = {
+		Void = {
+			Ores = {
+				Scribbal = 175000000;
+				Vantivium = 1120000;
+				Inkonium = 48400000;
+				Pasivium = 17200000;
+				Erodimium = 30300000;
+				Jet = 200000;
+				Blazuine = 4200000;
+				["Sentient Viscera"] = 28000000;
+				Nyctophyte = 475000000;
+			};
+			MainOre = "Voidstone";
+			Rarity = 35;
+		};
+		Divine = {
+			Ores = {
+				["Chroma Contaris"] = 3000;
+				Aether = 14000000;
+				Lucidium = 32100123;
+				Neptunium = 2920000;
+				Luminatite = 25400000;
+				Machina = 37700000;
+			};
+			MainOre = "Etherstone";
+			Rarity = 20;
+		};
+		Metallic = {
+			Rarity = 11;
+			Ores = {
+				Vulkavium = 16800000;
+				Elexinite = 1300000;
+				Nickel = 25;
+				Pyrite = 14250;
+				Thundarian = 1500000;
+				Copper = 30;
+				Gargantium = 62300000;
+				Polarium = 40000004;
+				Gold = 75;
+			};
+			MainOre = "Reflectistone";
+		};
+		Gilded = {
+			Ores = {
+				Gold = 20;
+				Ophanim = 377777;
+			};
+			Rarity = 5700;
+			MainOre = "Goldstone";
+		};
+		Prismatic = {
+			Ores = {
+				["\xCF\x80"] = 3141592;
+				Vitrilyx = 7000000;
+				Exoretic = 32000000;
+				Idolium = 145000000;
+				["Vaporwave Crystal"] = 75000000;
+				Rainbonite = 125000;
+				Pastelorium = 36600000;
+				Chromatite = 657000;
+				Bismuth = 1500;
+				Temporum = 3555555;
+			};
+			MainOre = "Prismatistone";
+			Rarity = 27;
+		};
+		Geode = {
+			Ores = {
+				Quartz = 30;
+				Scertanium = 2350000;
+				Ornalium = 6100000;
+				Speatrium = 18000000;
+				Tanzanite = 55000;
+				Painite = 16500;
+				Adurite = 35000;
+				Cobalt = 3000;
+				Viridian = 35000;
+				Emerald = 200;
+				Amber = 750;
+				Musgravite = 9500;
+				Albinite = 24242424;
+				["Elbrus' Pride"] = 51000000;
+				["Black Diamond"] = 30000;
+			};
+			Rarity = 12;
+			MainOre = "Crystallized Stone";
+		};
+		Elemental = {
+			Rarity = 17;
+			Ores = {
+				Freon = 2333333;
+				Intoxium = 25000;
+				Coldfirium = 25000;
+				Elementium = 25000000;
+				Flaeon = 2333333;
+				Trinitium = 33333333;
+				Poiseon = 2333333;
+				Incinderium = 25000;
+			};
+			MainOre = "Celestone";
+		};
+		Frozen = {
+			Ores = {
+				Freon = 2333333;
+				Cryotic = 206000;
+				Glacielle = 25500000;
+				Altovite = 4100000;
+				Cerlustrium = 25600000;
+				Coldfirium = 45000;
+				Inclemetite = 300000000;
+				Snoblintium = 3120000;
+			};
+			MainOre = "Ice";
+			Rarity = 8;
+		};
+	};
+	Tutorial = {
+		Metallic = {
+			Rarity = 4;
+			Ores = {
+				Gold = 75;
+				Copper = 30;
+				Pyrite = 14250;
+				Nickel = 25;
+			};
+			MainOre = "Reflectistone";
+		};
+	};
+}
+
+-- AST to string done in: 0.003104 secondsS
